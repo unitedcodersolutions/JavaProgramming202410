2c91acb46624c0e201b1e65c8b6aa03def4d92f0
DolkunTarim <dolkunt@gmail.com>
2024-10-18 14:33:30 -0400

    Initial Commit

---
+++ src/main/java/com/unitedcoder/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.unitedcoder;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to United Coder Java Programming Training");
+        // This application program displays Hello World
+        /* This application program displays Hello World */
+        /*
+        This application program
+        displays Hello World
+         */
+
+        System.out.println("Hello World");
+
+        //System.out.println("جاۋا پىروگىراممسىدا ئۇيغۇرچىنى چىقىرىش ");
+        //System.out.println("\uFE8B"+"\uFE8E"+"\uFEE7"+"\uFE8E");
+        //System.out.println("\uFE97"+"\uFBE9"+"\uFEDE");
+    }
+}
+
