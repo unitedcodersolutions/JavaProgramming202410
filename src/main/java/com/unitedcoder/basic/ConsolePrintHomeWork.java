cd49378f75cd2f6259a87a973825d71559024f37
DolkunTarim <dolkunt@gmail.com>
2024-10-22 22:02:02 -0400

    Homework code

---
+++ src/main/java/com/unitedcoder/basic/ConsolePrintHomeWork.java
@@ -0,0 +1,9 @@
+package com.unitedcoder.basic;
+
+public class ConsolePrintHomeWork {
+    public static void main(String[] args) {
+        System.out.println("1 + 2 + 3");
+        System.out.println(1 + 2 + 3);
+    }
+}
+
